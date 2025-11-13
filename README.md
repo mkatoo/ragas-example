09b46d97e6c0a3ed00760092cd72be381e8f3659
mkatoo <28562951+mkatoo@users.noreply.github.com>
2025-11-14 00:34:56 +0900

    first

---
+++ README.md
@@ -0,0 +1,15 @@
+# ragas-example
+
+example RAGAS using Gemini
+
+## Installation
+
+```bash
+uv sync
+```
+
+## Usage
+
+```bash
+uv run main.py
+```
---
+++ main.py
@@ -0,0 +1,38 @@
+from datasets import Dataset
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from ragas import evaluate
+from ragas.llms import LangchainLLMWrapper
+from ragas.metrics import AnswerRelevancy, Faithfulness
+
+
+def main():
+    dataset = Dataset.from_dict(
+        {
+            "question": ["What is the capital of France?"],
+            "answer": [
+                "The capital of France is Paris.",
+            ],
+            "contexts": [
+                [
+                    "France's capital city is Paris, known for its art, fashion, and culture.",
+                    "Berlin is the capital of Germany.",
+                ],
+            ],
+        }
+    )
+    llm = LangchainLLMWrapper(
+        ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite", temperature=0)
+    )
+    embedding = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001")
+    results = evaluate(
+        dataset,
+        metrics=[Faithfulness(), AnswerRelevancy()],
+        llm=llm,
+        embeddings=embedding,
+    )
+
+    print(results)
+
+
+if __name__ == "__main__":
+    main()
